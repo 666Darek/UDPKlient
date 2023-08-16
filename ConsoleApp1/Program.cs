576c1e87c8e17199542c22969929f374859d9a73
666Darek <94192736+666Darek@users.noreply.github.com>
2023-08-16 13:56:25 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        int a = 0;
+        UdpCommunicator communicator = new UdpCommunicator(8000); // nasłuchuje na porcie 8000
+
+        // Rozpocznij asynchroniczne nasłuchiwanie w tle
+        communicator.TaskReceive();
+        // ... jakikolwiek inny kod ...
+        //communicator.StopListening();
+
+        Console.WriteLine("Nasłuchiwanie w tle rozpoczęte...");
+
+        while (true)
+        {
+            // Nadawanie wiadomości co 10 sekund
+            communicator.Send("Hello!", "127.0.0.1", 8000);
+            Console.WriteLine("Wysłano wiadomość.");
+            Thread.Sleep(10000); // Czekaj 10 sekund
+            a++;
+            if(a == 2)
+            {
+                communicator.StopListening();
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/UdpCommunicator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class UdpCommunicator
+{
+    private UdpClient udpClient;
+    private IPEndPoint endPoint;
+    private CancellationTokenSource cts = new CancellationTokenSource();
+
+    public UdpCommunicator(int localPort)
+    {
+        udpClient = new UdpClient(localPort);
+    }
+
+    public void Send(string message, string remoteIpAddress, int remotePort)
+    {
+        byte[] bytesToSend = Encoding.UTF8.GetBytes(message);
+        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(remoteIpAddress), remotePort);
+        udpClient.Send(bytesToSend, bytesToSend.Length, remoteEndPoint);
+    }
+
+    public string Receive()
+    {
+        endPoint = new IPEndPoint(IPAddress.Any, 0);
+        byte[] receivedBytes = udpClient.Receive(ref endPoint);
+        return Encoding.UTF8.GetString(receivedBytes);
+    }
+
+    public async Task<string> ReceiveAsync(CancellationToken cancellationToken = default)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            endPoint = new IPEndPoint(IPAddress.Any, 0);
+            UdpReceiveResult result = await udpClient.ReceiveAsync();
+            return Encoding.UTF8.GetString(result.Buffer);
+        }
+
+        return null;  // Zwróć null lub odpowiednią wiadomość w przypadku anulowania
+    }
+
+    public void TaskReceive()
+    {
+        Task.Run(async () =>
+        {
+            while (!cts.Token.IsCancellationRequested)
+            {
+                string receivedMessage = await ReceiveAsync(cts.Token);
+                if (receivedMessage != null)
+                {
+                    Console.WriteLine($"Otrzymano: {receivedMessage}");
+                }
+            }
+        });
+    }
+
+    public void StopListening()
+    {
+        cts.Cancel();
+    }
+
+    public void Close()
+    {
+        StopListening();
+        udpClient.Close();
+    }
+}
